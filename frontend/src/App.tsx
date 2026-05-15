2e4e39555f3de03b379d8216f02fcf4912019f71
Ivan <ivan@MacBook-Air-Ivan.local>
2026-05-15 21:39:32 +0200

    frontend: error display, custom components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import $api from "./lib/client";
+import Button from "./components/button";
 
 function App() {
 
-  const { mutate, isPending, data } = $api.useMutation("post", "/api/LinkItems",
+  const { mutate, isPending, data,error } = $api.useMutation("post", "/api/LinkItems",
     {
       onError: (err) => { console.log({ err }) }
     },
@@ -25,14 +26,27 @@ function App() {
   if (isPending) return <p>Loading...</p>
 
   return (
-    <div className="flex w-dvh h-dvh ">
+    <div className="flex flex-col w-dvw h-dvh justify-center items-center">
       <form onSubmit={e => HandleSubmit(e)}
-        className="shadow-2xl w-fit h-fit p-5 flex flex-col"
+        className="shadow-2xl
+        w-fit
+        h-fit
+        p-5
+        flex
+        flex-col
+        items-center
+        gap-4
+        border-2
+      border-stone-100
+        "
         >
-        <h1>Shorten a long link</h1>
-        <input placeholder="https://examplelonglink.com" ref={urlRef} className="border"></input>
+        <h1 className="text-2xl">Shorten a long link</h1>
+        <input placeholder="https://example-long-link.com" ref={urlRef} className=" p-2 text-2xl rounded-xl border border-stone-200"></input>
+        <Button label="Submit"/>
+
+        <h1>{error}</h1>
+        <h1>{data?.url}</h1>
       </form>
-      <h1>{data?.url}</h1>
     </div>
   )
 }
--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -2,10 +2,10 @@ import type React from "react"
 
 interface ButtonProps{
     label: string
-    type: "submit" | "reset"  | "button" | undefined
-    onClick: () => {}
-    ref: React.Ref<HTMLButtonElement> | undefined
-    className: string
+    type?: "submit" | "reset"  | "button" | undefined
+    onClick?: () => {}
+    ref?: React.Ref<HTMLButtonElement> | undefined
+    className?: string
 }
 
 export default function Button({label,className,type,onClick,ref} : ButtonProps){
@@ -13,6 +13,14 @@ export default function Button({label,className,type,onClick,ref} : ButtonProps)
     type={type}
     onClick={onClick}
     ref={ref}
-    className={"p-1 bg-green-400 " + " " + className}
+    className={`
+        p-4
+        bg-green-400
+        cursor-pointer
+        w-fit
+        text-2xl
+        rounded-xl
+        `
+         + " " + className}
     >{label}</button>
 }
